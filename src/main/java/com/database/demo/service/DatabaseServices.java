9b1da0b3f35d13386c5352c710235c4e33d28f30
sujanshrestha <shrestha94.sujan@gmail.com>
2021-05-04 10:25:33 -0500

    added generic method in the interface to return all author or all books

--- src/main/java/com/database/demo/service/DatabaseServices.java
+++ src/main/java/com/database/demo/service/DatabaseServices.java
@@ -1,8 +1,10 @@
 package com.database.demo.service;
 
+import java.util.List;
+
 public interface DatabaseServices {
     public <T> void add(T obj);
     public Object get(int id);
     public void delete(int id);
-
+    public Object getAll();
 }
